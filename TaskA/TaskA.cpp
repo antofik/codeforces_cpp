1b410529f6486f9d764f72c6e7c5521874c3bfbe
Anton Naumenko <antofik@gmail.com>
2019-06-06 08:49:48 +0300

    A+

--- TaskA/TaskA.cpp
+++ TaskA/TaskA.cpp
@@ -2,7 +2,7 @@
 
 #include "Library.h"
 
-ULL dp[100000 + 2][202][2];
+ULL dp[100000 + 2][2][202];
 
 #define MEMSET(a,x) memset(&a,x,sizeof(a));
 #define MEMZERO(a) MEMSET(a,0)
@@ -24,33 +24,37 @@ void task()
 	vector<int> a(n + 2);
 	REP(i, n) scanf("%d", &a[i]);
 
-	// dp[i][h][0] - how many combinations before a[i] when a[i+1]==h and a[i+2] < a[i+1]
-	// dp[i][h][1] - how many combinations before a[i] when a[i+1]==h and a[i+2] >= a[i+1]
+	// dp[i][0][h] - how many combinations before a[i] when a[i+1]==h and a[i+2] < a[i+1]
+	// dp[i][1] [h]- how many combinations before a[i] when a[i+1]==h and a[i+2] >= a[i+1]
 
 	REP(h, H) {
-		dp[0][h][1] = 1;
-		dp[0][h][0] = 0;
+		dp[0][1][h] = 1;
+		dp[0][0][h] = 0;
 	}
-	dp[0][1][0] = 1;
+	dp[0][0][1] = 1;
 
 	REP(i, n) {
-		//printf("--- i=%d a[i]=%d ----\n", i, a[i]);
-		REP(h, H) {
-			if (a[i] == -1) {
-				REP(k, H) dp[i][h][1] += dp[i - 1][k][h >= k];
-				REP(k, h, H) dp[i][h][0] += dp[i - 1][k][h >= k];
+		ULL s0[202] = { 0 };
+		ULL s1[202] = { 0 };
+
+		REP(h, H)  s1[h] = (s1[h - 1] + dp[i - 1][1][h]) % MOD;
+		REPR(h, H) s0[h] = (s0[h + 1] + dp[i - 1][0][h]) % MOD;
+
+		if (a[i] == -1) {
+			REP(h, H) {
+				dp[i][1][h] = (s1[h] + s0[h + 1]) % MOD;
+				dp[i][0][h] = (dp[i - 1][1][h] + s0[h + 1]) % MOD;
 			}
-			else {
-				REP(h, H) {
-					dp[i][h][1] = dp[i - 1][a[i]][h >= a[i]];
-					dp[i][h][0] = h <= a[i] ? dp[i - 1][a[i]][h >= a[i]] : 0;
-				}
+		}
+		else {
+			REP(h, H) {
+				dp[i][1][h] = dp[i - 1][h >= a[i]][a[i]];
+				dp[i][0][h] = h <= a[i] ? dp[i - 1][h >= a[i]][a[i]] : 0;
 			}
-			//if (h < 5) printf("h=%d\t%lld\t%lld\n", h, dp[i][h][0], dp[i][h][1]);
 		}
 	}
 
-	printf("%lld", dp[n][1][0]);
+	printf("%lld", dp[n][0][1]);
 }
 
 #endif
